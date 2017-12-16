eb99882e3aceed10e049b305ad0fbc4728db108b
ciou <yyouko22@gmail.com>
2017-12-16 16:42:11 +0800

    change word to english

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,54 +10,54 @@ puts "Admin已建好，帳號為#{u.email},密碼為#{u.password}"
 # Initial Video
 Video.create!(wistia: "8ooc2jvwoo",
   title: "dark-1",
-  description: "dark-1",
+  description: "description-dark-1",
   image: open("app/assets/images/dark1.png")
 )
 
 Video.create!(wistia: "hu3msaqtpo",
   title: "fight-1",
-  description: "fight-1",
+  description: "description-fight-1",
   image: open("app/assets/images/fight1.png")
 )
 
 Video.create!(wistia: "2uifiadmaj",
   title: "duck-1",
-  description: "duck-1",
+  description: "description-duck-1",
   image: open("app/assets/images/duck1.png")
 )
 
 Video.create!(wistia: "8ooc2jvwoo",
   title: "dark-2",
-  description: "dark-2",
+  description: "description-dark-2",
   image: open("app/assets/images/dark2.png")
 )
 
 Video.create!(wistia: "hu3msaqtpo",
   title: "fight-2",
-  description: "fight-2",
+  description: "description-fight-2",
   image: open("app/assets/images/fight2.png")
 )
 
 Video.create!(wistia: "2uifiadmaj",
   title: "duck-2",
-  description: "duck-2",
+  description: "description-duck-2",
   image: open("app/assets/images/duck2.png")
 )
 
 Video.create!(wistia: "8ooc2jvwoo",
   title: "dark-3",
-  description: "dark-3",
+  description: "description-dark-3",
   image: open("app/assets/images/dark3.png")
 )
 
 Video.create!(wistia: "hu3msaqtpo",
   title: "fight-3",
-  description: "fight-3",
+  description: "description-fight-3",
   image: open("app/assets/images/fight2.png")
 )
 
 Video.create!(wistia: "2uifiadmaj",
   title: "duck-3",
-  description: "duck-3",
+  description: "description-duck-3",
   image: open("app/assets/images/duck3.png")
 )
