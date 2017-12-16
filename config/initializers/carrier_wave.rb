8ecb1748bbda221c8ebb263f10e43213379319a4
ciou <yyouko22@gmail.com>
2017-12-16 21:58:59 +0800

    fix config.fog bug

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,15 +1,17 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_key_id'],
       aws_secret_access_key: ENV['S3_key_secret'],
-      region:                'ap-northeast-1'
+      region:                ENV['S3_region']
     }
-    config.fog_directory = 'video-place'
+    config.fog_directory = ENV['S3_bucket']
+    config.fog_public = false
+    config.storage = :fog
 
   else
-    config.storage :file
+    config.storage = :file
   end
 end
